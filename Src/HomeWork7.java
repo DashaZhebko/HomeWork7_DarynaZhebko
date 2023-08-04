1ea492c0b14c036aa9d05c0708841b46f994e5e4
DashaZhebko <daryna.zhebko@gmail.com>
2023-08-04 21:20:01 +0300

    second commit

--- Src/HomeWork7.java
+++ Src/HomeWork7.java
@@ -1,5 +1,6 @@
 public class HomeWork7 {
     public static void main(String[] args) {
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git!!!");
+        System.out.println("Hello from Daryna!");
     }
 }
